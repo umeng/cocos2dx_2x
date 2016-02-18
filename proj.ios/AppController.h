0306078bfd4bd5cc54b76a6d8f37d6506675d5cf
yuqiang.wyq <yuqiang.wyq@alibaba-inc.com>
2016-02-18 16:47:34 +0800

    fix #7900420 twitter第三方SDKwebview授权， crash

--- proj.ios/AppController.h
+++ proj.ios/AppController.h
@@ -3,9 +3,9 @@
 @class RootViewController;
 
 @interface AppController : NSObject <UIApplicationDelegate> {
-    UIWindow *window;
     RootViewController    *viewController;
 }
 
+@property (nonatomic, strong) UIWindow *window;
 @end
 
